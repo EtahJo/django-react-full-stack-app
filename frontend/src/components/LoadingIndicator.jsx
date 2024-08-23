fbba2663373c85877176c44348d438fb6597eb71
etah <arrahetah23@gmail.com>
2024-08-23 10:51:10 +0100

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/loadingIndicator.css'
+
+function LoadingIndicator() {
+  return (
+    <div className='loading-container'>
+        <div className='loader'/>
+    </div>
+  )
+}
+
+export default LoadingIndicator
